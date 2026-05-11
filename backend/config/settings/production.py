9ecf06303db6e6abac1152ebb536f72ce19241c8
Mahesh-363 <umamahesh7901367554@gmail.com>
2026-05-12 00:33:11 +0530

    debug: allow all CORS origins temporarily

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -47,7 +47,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'contenttype',
 ]
