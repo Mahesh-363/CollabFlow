e77131e4f515f3a95b22072963ae6ae77f37d96d
Mahesh-363 <umamahesh7901367554@gmail.com>
2026-05-14 13:52:06 +0530

    fix: remove >>> from echo in build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,7 @@
 @"
 #!/usr/bin/env bash
 set -o errexit
-echo ">>> DATABASE_URL = $DATABASE_URL"
+echo "DATABASE_URL = $DATABASE_URL"
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
