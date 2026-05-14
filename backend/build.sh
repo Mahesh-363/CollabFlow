b3374dee3f7f5e777d533d43b9d86131cc3184f8
Mahesh-363 <umamahesh7901367554@gmail.com>
2026-05-14 13:54:44 +0530

    fix: rewrite build.sh correctly

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,8 @@
-@"
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 set -o errexit
-echo "DATABASE_URL = $DATABASE_URL"
+echo DATABASE_URL
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py seed_demo
-"@ | Set-Content "F:\Main Projects\collabflow\backend\build.sh" -Encoding UTF8
